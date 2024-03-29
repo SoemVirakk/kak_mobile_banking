e1420a809d559043b0e0daeedd639d3069442330
it.chhaya <it.chhaya@gmail.com>
2024-03-29 08:47:02 +0700

    Initial commit

---
+++ src/main/java/co/istad/mbanking/domain/Role.java
@@ -0,0 +1,29 @@
+package co.istad.mbanking.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+
+    @ManyToMany
+    private List<Authority> authorities;
+
+}
