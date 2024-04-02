b2909aefcd3fddee93f039d0d4fa6d6ad4da7f96
it.chhaya <it.chhaya@gmail.com>
2024-04-02 09:14:42 +0700

    upload single file

---
+++ src/main/java/co/istad/mbanking/features/media/dto/MediaResponse.java
@@ -0,0 +1,13 @@
+package co.istad.mbanking.features.media.dto;
+
+import lombok.Builder;
+
+@Builder
+public record MediaResponse(
+        String name, // unique
+        String contentType, // PNG, JPEG, MP4
+        String extension,
+        String uri, // https://api.istad.co/media/image/899bac49-e47c-406c-abb2-30ad0b498f88.png
+        Long size
+) {
+}
