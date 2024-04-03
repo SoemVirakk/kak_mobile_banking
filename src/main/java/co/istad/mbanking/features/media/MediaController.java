fe6e7f0944d79a6c124cde76aa1b0a12da2b8253
it.chhaya <it.chhaya@gmail.com>
2024-04-03 10:07:40 +0700

    upload multiple, load file and delete file

--- src/main/java/co/istad/mbanking/features/media/MediaController.java
+++ src/main/java/co/istad/mbanking/features/media/MediaController.java
@@ -2,11 +2,11 @@ package co.istad.mbanking.features.media;
 
 import co.istad.mbanking.features.media.dto.MediaResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/medias")
@@ -15,9 +15,26 @@ public class MediaController {
 
     private final MediaService mediaService;
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/upload-single")
     MediaResponse uploadSingle(@RequestPart MultipartFile file) {
         return mediaService.uploadSingle(file, "IMAGE");
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/upload-multiple")
+    List<MediaResponse> uploadMultiple(@RequestPart List<MultipartFile> files) {
+        return mediaService.uploadMultiple(files, "IMAGE");
+    }
+
+    @GetMapping("/{mediaName}")
+    MediaResponse loadMediaByName(@PathVariable String mediaName) {
+        return mediaService.loadMediaByName(mediaName, "IMAGE");
+    }
+
+    @DeleteMapping("/{mediaName}")
+    MediaResponse deleteMediaByName(@PathVariable String mediaName) {
+        return mediaService.deleteMediaByName(mediaName, "IMAGE");
     }
 
 }
